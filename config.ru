5f1e08ddf589fa7a4f8f9081ab99037aa24037eb
Erik Kastner <kastner@gmail.com>
2009-11-02 09:57:30 -0500

    mostly working

--- config.ru
+++ config.ru
@@ -1,4 +1,9 @@
 require 'sinatra'
+require 'mustache/sinatra'
 require 'xtract.rb'
+require 'lib/amazon_zoom_extractor'
 
+Sinatra::Application.class_eval do
+  register Mustache::Sinatra
+end
 run Sinatra::Application
--- xtract.rb
+++ xtract.rb
@@ -1,3 +1,14 @@
 get "/" do
-  "Hi there"
+  mustache :index
+end
+
+post "/extract" do
+  if !params[:url].empty? && !params[:file_name].empty?
+    public = File.join(File.dirname(__FILE__)) + "/public"
+    file_name = "/extracted/" + params[:file_name].gsub(/[^a-zA-Z0-9_-]/,'') + ".jpg"
+    AmazonZoomExtractor.extract(params[:url], "#{public}#{file_name}")
+    redirect file_name
+  else
+    "Sorry. You have to supply the url and name"
+  end
 end
